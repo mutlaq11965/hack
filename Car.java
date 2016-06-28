30773364424411ce508a88082d0363bb707e654f
mutlaq alghusson <mutlaq11965@gmail.com>
2016-06-28 10:03:44 -0500

    no message

--- Car.java
+++ Car.java
@@ -1,15 +1,35 @@
-package Cars;
 import processing.core.PApplet;
- 
 
 public class Car {
-	PApplet p;
-	public Car(PApplet np) 
-	{
-		p = np;
-	}
-	public int do_somthing(){
-		p.line(100,100,50,50);
-		return 3;
-	}
+
+	// Class variables (data)
+			float x;
+			float y;
+			int c;
+			float speed;
+			PApplet processingContext;
+			
+			// Class Constructor
+			public Car( PApplet newProcessingContext, float nx, float ny, float nspeed){
+				processingContext = newProcessingContext;
+				x = nx;
+				y = ny;
+				speed = nspeed;
+				c = (int)processingContext.random(256);
+			}
+			
+			// Display the car (functionality)
+			public void display(){
+				processingContext.fill(c);
+				processingContext.rect(x,y,30,10);
+			}
+			
+			// Move the car (functionality)
+			public void move(){
+				if(x > processingContext.width){
+					x = -30;
+				}
+				x=x+speed;
+			}
+
 }
